bf85d8452f9c4c936ff22a3c31cddc2173033b16
SoftwareMatrix <ilyaphestvenidze@gmail.com>
2026-06-11 15:49:59 +0400

    Add navbar with isolated styles

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+const Navbar = () => {};
+
+return (
+  <nav className="navbar">
+    <h2 className="navbar-logo">Verifly</h2>
+  </nav>
+);
+
+export default Navbar;
